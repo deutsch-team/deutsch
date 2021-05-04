9d280ff43737cbac0bdc372c0eb4e9efb8dcdd5f
Jakob Wilms <jakob.wilms06@t-online.de>
2021-05-04 21:27:26 +0200

    cmdschreibe.py, added comments

--- deutsch alpha_1/deutsch/cmdschreibe.py
+++ deutsch alpha_1/deutsch/cmdschreibe.py
@@ -1,20 +1,20 @@
 def cmd_schreibe(args):
-    if args[0] == '"' and args[-1] == 0:
-        print(''.join(args[1:-1]))
+    if args[0] == '"' and args[-1] == '"':  # String
+        print(''.join(args[1:-1]))  # Print without '"`
     else:
         number = True
         point = False
         numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        for arg in args:
-            if arg not in numbers:
-                if arg == ".":
-                    if not point:
+        for arg in args:  # Iterate through all characters in args
+            if arg not in numbers:  # If arg isn't a number
+                if arg == ".":  # Float/Double
+                    if not point:  # There can only be one '.'
                         point = True
                     else:
                         number = False
-                        break
+                        break  # Exit Loop
                 else:
-                    number = False
-                    break
-        if number:
+                    number = False  # Else it isn't a number
+                    break  # Exit Loop
+        if number:  # Print only if it is a number
             print(str(args))
